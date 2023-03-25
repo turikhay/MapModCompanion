32979fd8e5cb2e1af6059bdc7bce9970b994758f
turikhay <git@turikhay.com>
2023-03-25 16:37:13 +0300

    decouple single handler init

--- common/src/main/java/com/turikhay/mc/mapmodcompanion/Handler.java
+++ common/src/main/java/com/turikhay/mc/mapmodcompanion/Handler.java
@@ -1,5 +1,6 @@
 package com.turikhay.mc.mapmodcompanion;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -8,18 +9,24 @@ public interface Handler extends Disposable {
     static <PluginType> List<Handler> initialize(Logger logger, PluginType plugin, List<Factory<PluginType>> factories) {
         ArrayList<Handler> handlers = new ArrayList<>();
         for (Factory<PluginType> factory : factories) {
-            logger.fine("Calling the handler factory: " + factory.getName());
-            Handler handler;
-            try {
-                handler = factory.create(plugin);
-            } catch (InitializationException e) {
-                logger.info(factory.getName() + " handler will not be available (" + e.getMessage() + ")");
-                continue;
-            }
+            Handler handler = initialize(logger, plugin, factory);
             handlers.add(handler);
-            logger.fine("Handler has been initialized: " + handler);
         }
         return handlers;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    static <HandlerType, PluginType> @Nullable HandlerType initialize(Logger logger, PluginType plugin, Factory<PluginType> factory) {
+        logger.fine("Calling the handler factory: " + factory.getName());
+        Handler handler;
+        try {
+            handler = factory.create(plugin);
+        } catch (InitializationException e) {
+            logger.info(factory.getName() + " handler will not be available (" + e.getMessage() + ")");
+            return null;
+        }
+        logger.fine("Handler has been initialized: " + handler);
+        return (HandlerType) handler;
     }
 
     static void cleanUp(Logger logger, List<Handler> handlers) {
