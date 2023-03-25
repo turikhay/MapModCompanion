512b7051bc5310901f1cf7da0a8f04f612a5d5bb
turikhay <git@turikhay.com>
2023-03-25 19:18:13 +0300

    fix protocol version for modern

--- tests_e2e/bot/tests/world_id/modern.mjs
+++ tests_e2e/bot/tests/world_id/modern.mjs
@@ -4,7 +4,7 @@ import test from "./common.mjs";
 export default {
   groups: ["voxelmap", "journeymap"],
   test: (/** @type Client */ client, /** @type number */ protocolVersion) =>
-    protocolVersion > 757 // >= 1.16.4
+    protocolVersion >= 754 // >= 1.16.4
       ? test(client, "worldinfo:world_id", {
         request: [0, 42, 0],
         response: [42, 1, 49],
