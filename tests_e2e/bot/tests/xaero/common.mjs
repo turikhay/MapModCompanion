2223bc8504d44e645ed6ecf80eb9479ec93c1783
turikhay <git@turikhay.com>
2023-03-31 23:46:59 +0300

    Tests: bot: construct response buffer

--- tests_e2e/bot/tests/xaero/common.mjs
+++ tests_e2e/bot/tests/xaero/common.mjs
@@ -1,5 +1,8 @@
 import { Client } from "minecraft-protocol";
 
+const expectedResponseBuffer = Buffer.alloc(5);
+expectedResponseBuffer.writeInt32BE(1337, 1);
+
 export default function test(
   /** @type Client */ client,
   /** @type string */ channel
@@ -12,7 +15,7 @@ export default function test(
   return new Promise((resolve, reject) => {
     // validate incoming packets in our channel
     client.on(channel, (/** @type Buffer */ buffer) => {
-      const isExpected = buffer.equals(Buffer.from([0, 0, 0, 5, 57]));
+      const isExpected = buffer.equals(expectedResponseBuffer);
       if (isExpected) {
         resolve();
       } else {
