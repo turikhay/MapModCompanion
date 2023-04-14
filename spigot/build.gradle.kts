20301470112ade46e9b4775876150fb946d1c69b
turikhay <git@turikhay.com>
2023-04-14 20:44:53 +0300

    Migrate spigot

---
+++ spigot/build.gradle.kts
@@ -0,0 +1,55 @@
+import kr.entree.spigradle.kotlin.spigot
+
+plugins {
+    java
+    alias(libs.plugins.spigradle)
+    alias(libs.plugins.shadow)
+}
+
+apply {
+    from("$rootDir/gradle/java.gradle")
+    from("$rootDir/gradle/shadow.gradle")
+}
+
+repositories {
+    maven {
+        url = uri("https://repo.dmulloy2.net/repository/public/")
+    }
+}
+
+val bStats = with(libs.bstats.bukkit.get()) { "$module:$versionConstraint" }
+val spigot_version: String by project
+val protocolLib_version: String by project
+
+spigot {
+    name = "MapModCompanion"
+    authors = listOf("turikhay")
+    apiVersion = "1.13"
+    softDepends = listOf("ProtocolLib")
+    debug {
+        jvmArgs = listOf(
+                "-Xmx2048m",
+                "-Dlog4j.configurationFile=${projectDir}${File.separatorChar}log4j2-debug.xml",
+                "-Dcom.turikhay.mc.mapmodcompanion.spigot.debug=true"
+        )
+    }
+    afterEvaluate {
+        excludeLibraries = listOf(
+                rootProject.allprojects.map { "$group:$name:$version" },
+                listOf(bStats)
+        ).flatten()
+    }
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation(libs.bstats.bukkit)
+
+    // These dependencies are intentionally not present in libs.version.toml
+    compileOnly(spigot(spigot_version))
+    compileOnly("com.comphenix.protocol:ProtocolLib:${protocolLib_version}")
+}
+
+tasks.shadowJar {
+    archiveFileName.set("MapModCompanion-Spigot.jar")
+}
