cfa0d4023da96a7c73f55d13943c201dfc8ba79a
turikhay <git@turikhay.com>
2023-04-14 20:45:11 +0300

    Migrate root

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    java
+    alias(libs.plugins.spigradle) apply false
+    alias(libs.plugins.spigradle.bungee) apply false
+    alias(libs.plugins.shadow) apply false
+}
+
+group "com.turikhay.mc"
