eba331c56680a351f7fb419d4aa24e74ac3e2c51
turikhay <git@turikhay.com>
2023-04-14 20:44:35 +0300

    Migrate bungee

---
+++ bungee/build.gradle.kts
@@ -0,0 +1,30 @@
+import kr.entree.spigradle.kotlin.bungeecord
+
+plugins {
+    java
+    alias(libs.plugins.spigradle.bungee)
+    alias(libs.plugins.shadow)
+}
+
+apply {
+    from("$rootDir/gradle/java.gradle")
+    from("$rootDir/gradle/shadow.gradle")
+}
+
+bungee {
+    name = "MapModCompanion"
+    author = "turikhay"
+    debug {
+        jvmArgs = listOf("-Xmx256m", "-Dnet.md_5.bungee.console-log-level=ALL")
+    }
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation(libs.bstats.bungeecord)
+    compileOnly(bungeecord())
+}
+
+tasks.shadowJar {
+    archiveFileName.set("MapModCompanion-Bungee.jar")
+}
