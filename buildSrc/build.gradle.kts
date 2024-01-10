6795faaba7018f881f0357fc4a3cca1dd07847cd
turikhay <git@turikhay.com>
2024-01-11 01:08:34 +0300

    PluginDescriptorTask

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,4 +10,9 @@ dependencies {
     // See https://github.com/gradle/gradle/issues/15383
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
     implementation(with(libs.plugins.shadow.get()) { "com.github.johnrengelman:shadow:$version" })
+    with(libs.versions.jackson) {
+        val version = get()
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$version")
+        implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$version")
+    }
 }
