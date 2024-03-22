5685fb7ceaef5c3c6973351b499553a7426c0ae3
turikhay <git@turikhay.com>
2024-03-23 00:21:48 +0300

    deduplicate shadowJar output

--- buildSrc/src/main/kotlin/ProjectExt.kt
+++ buildSrc/src/main/kotlin/ProjectExt.kt
@@ -1,5 +1,17 @@
 import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.TaskOutputs
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.the
+import java.io.File
 
 // See https://github.com/gradle/gradle/issues/15383
 val Project.libs get() = the<org.gradle.accessors.dm.LibrariesForLibs>()
+
+val Task.singleFile get() = outputs.files.singleFile
+
+val <T : Task?> TaskProvider<T>.singleFile get(): Provider<File> = this.map {
+    @Suppress("UNNECESSARY_NOT_NULL_ASSERTION")
+    it!!.outputs.files.singleFile
+}
--- buildSrc/src/main/kotlin/java-shadow.gradle.kts
+++ buildSrc/src/main/kotlin/java-shadow.gradle.kts
@@ -8,6 +8,28 @@ tasks {
         dependsOn(shadowJar)
     }
 
+    val createDedupJar = register<Jar>("createDedupJar") {
+        from(shadowJar.map { zipTree(it.singleFile) })
+        archiveFileName = shadowJar.map {
+            it.archiveFileName.get().replace(".jar", "-dedup.jar")
+        }
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+
+    val copyBackDedupJar = register("copyBackDedupJar") {
+        dependsOn(createDedupJar)
+        doLast {
+            createDedupJar.singleFile.get().copyTo(
+                    shadowJar.singleFile.get(),
+                    overwrite = true
+            )
+        }
+    }
+
+    val dedupShadowJar = register("dedupShadowJar") {
+        dependsOn(shadowJar, createDedupJar, copyBackDedupJar)
+    }
+
     shadowJar {
         archiveFileName = "MapModCompanion-${
             project.name.replaceFirstChar {
@@ -20,5 +42,7 @@ tasks {
         ).forEach { pkg ->
             relocate(pkg, with(rootProject) { "${group}.${name}.shade.${pkg}" })
         }
+
+        finalizedBy(dedupShadowJar)
     }
 }
