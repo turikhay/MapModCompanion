99a98cfb16cc3a9d94d9556af0e5f8849166e027
turikhay <git@turikhay.com>
2024-09-16 09:25:58 +0400

    change logging level to info

--- spigot/src/main/java/com/turikhay/mc/mapmodcompanion/spigot/MapModCompanion.java
+++ spigot/src/main/java/com/turikhay/mc/mapmodcompanion/spigot/MapModCompanion.java
@@ -130,7 +130,7 @@ public class MapModCompanion extends JavaPlugin {
         }
         IdRegistry registry;
         if (world == null) {
-            logger.severe("For every world plugin will now send their unique IDs");
+            logger.info("For every world plugin will now send their unique IDs");
             registry = new IdRegistry.DynamicUUIDRegistry();
         } else {
             int id = world.getUID().hashCode();
@@ -155,7 +155,7 @@ public class MapModCompanion extends JavaPlugin {
             if (env == World.Environment.NORMAL) {
                 if (defaultWorld != null) {
                     // Non-default server configuration
-                    logger.severe("Unexpected world: " + world);
+                    logger.info("Unexpected world: " + world);
                     return null;
                 }
                 defaultWorld = world;
