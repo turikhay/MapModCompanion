733c2861170cb17db2ece74316c4bd8e654c8ed1
turikhay <git@turikhay.com>
2025-08-07 18:24:06 +0500

    remove unnecessary loop

--- tests_e2e/run.py
+++ tests_e2e/run.py
@@ -466,13 +466,11 @@ if __name__ == "__main__":
         *(['--detach'] if auto else []),
     ])
 
-    while True:
-        try:
-            exit_code = docker_proc.wait()
-            break
-        except KeyboardInterrupt:
-            docker_proc.send_signal(SIGINT)
-            exit(1)
+    try:
+        exit_code = docker_proc.wait()
+    except KeyboardInterrupt:
+        docker_proc.send_signal(SIGINT)
+        exit(1)
 
     if auto:
         logger.info(f"Waiting for {bot_container}")
