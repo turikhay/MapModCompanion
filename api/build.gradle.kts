40a66998fad5fa5294dd4a4bb7c70a879c2afa90
turikhay <git@turikhay.com>
2025-08-09 23:18:41 +0500

    Publish to GitHub packages

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -9,9 +9,30 @@ java {
 }
 
 publishing {
+    fun env(name: String) = System.getenv(name) ?: name
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${env("GITHUB_REPOSITORY")}")
+            credentials {
+                username = env("GITHUB_ACTOR")
+                password = env("GITHUB_TOKEN")
+            }
+        }
+    }
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
+            pom {
+                name = "MapModCompanion (API)"
+                artifactId = "mapmodcompanion-api"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+            }
         }
     }
 }
